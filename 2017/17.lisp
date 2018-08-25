027c6272d27c2eb57e21f0a66bc51d851a2b4f72
Javier Olaechea <pirata@gmail.com>
2018-08-25 17:12:15 -0500

    2017 day-17 part 1

--- 2017/17.lisp
+++ 2017/17.lisp
@@ -2,22 +2,41 @@
   (:use #:cl))
 (in-package #:aoc/17)
 
-(defun solve/1 (step times)
-  (let ((seq (vector 0))
-        (current-index 0)
-        (next-elt 1)
-        (next-index))
-    (dotimes (i times)
-      (setf next-index (mod (+ current-index step)
-                            (length seq)))
-      (if (= (1+ next-index) (length seq))
-          (setf seq (concatenate 'vector seq `#(,next-elt)))
-          (setf seq (concatenate 'vector
-                                 (subseq seq 0 next-index)
-                                 `#(,next-elt)
-                                 (subseq seq next-index))))
-      (incf next-elt)
-      (setf current-index next-index))
-    (values seq current-index)))
 
-;; Not 135
+(defun insert (el seq position)
+  (let ((left-seq (subseq seq 0 position))
+        (right-seq (subseq seq position)))
+    (concatenate 'vector
+                 left-seq
+                 (vector el)
+                 right-seq)))
+
+(defun %step (seq current-index next-el stride target-el)
+  (if (> next-el target-el)
+      (values seq current-index (aref seq (1+ current-index)))
+      (let* ((next-index (mod (+ current-index stride)
+                              (length seq)))
+             (next-seq (insert next-el seq (1+ next-index))))
+        (%step next-seq (1+ next-index) (1+ next-el) stride target-el))))
+
+(defun solve-1 (step times)
+  (%step (vector 0) 0 1 step times))
+
+#+iterative
+(defun solve-iter (step times)
+  (loop
+    :with seq := (vector 0)
+    :with current-index := 0
+    :with next-el := 1
+    :repeat times
+    :do (let* ((next-index (mod (+ current-index step)
+                                (length seq)))
+               (next-seq (insert next-el seq (1+ next-index))))
+          (psetf seq next-seq
+                 current-index (1+ next-index)
+                 next-el (1+ next-el)))
+    :finally (return (values seq current-index (aref seq (1+ current-index))))))
+
+;; Part 1 -> 1173
+#+puzzle-2
+(solve-iter 304 50000000)
