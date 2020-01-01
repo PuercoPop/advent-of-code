cedcbfcacb415f471d79c9aeaf0cf3f413540cc1
Javier Olaechea <pirata@gmail.com>
2020-01-01 17:30:31 -0500

    Day 2 part 2

--- 2015/2.rs
+++ 2015/2.rs
@@ -9,9 +9,16 @@ fn area_smallest_side(l:u64, w:u64, h:u64) -> u64 {
     return cmp::min(h*w, cmp::min(l*w, l*h))
 }
 
-fn paper_requires(l:u64, w:u64, h:u64) -> u64 {
-    return surface_area(l, w, h) + area_smallest_side(l, w, h)
+fn smallest_perimeter(l:u64, w:u64, h:u64) -> u64 {
+    return cmp::min(h+h+w+w, cmp::min(l+l+w+w, l+l+h+h))
+}
 
+fn paper_required(l:u64, w:u64, h:u64) -> u64 {
+    return surface_area(l, w, h) + area_smallest_side(l, w, h)
+}
+
+fn ribbon_required(l:u64, w:u64, h:u64) -> u64 {
+    return smallest_perimeter(l,w,h) + l*w*h
 }
 
 fn parse_line(line:&str) -> (u64, u64, u64) {
@@ -23,10 +30,13 @@ fn parse_line(line:&str) -> (u64, u64, u64) {
 
 fn main() {
     let input = fs::read_to_string("2.input").unwrap();
-    let mut tot:u64 = 0;
+    let mut paper_total:u64 = 0;
+    let mut ribbon_total:u64 = 0;
     for line in input.lines() {
         let (length, width, height) = parse_line(line);
-        tot = tot + paper_requires(length, width, height);
+        paper_total = paper_total + paper_required(length, width, height);
+        ribbon_total = ribbon_total + ribbon_required(length, width, height);
     }
-    println!("Total: {}", tot)
+    println!("Total: {}", paper_total);
+    println!("Total: {}", ribbon_total);
 }
